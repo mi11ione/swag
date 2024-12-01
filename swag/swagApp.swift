0e095bde6173019108b659c60f2a5df9ede83e06
mi11ione <i@mi11ion.io>
2024-12-01 09:08:04 +0000

    Initial Commit

---
+++ swag/swagApp.swift
@@ -0,0 +1,17 @@
+//
+//  swagApp.swift
+//  swag
+//
+//  Created by mi11ion on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct swagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
