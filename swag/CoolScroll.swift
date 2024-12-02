db593836c5c4822120be873199105204e8b42a7d
mi11ion <122489852+mi11ione@users.noreply.github.com>
2024-12-02 20:01:15 +0300

    Update CoolScroll.swift
    Прикольно

--- swag/CoolScroll.swift
+++ swag/CoolScroll.swift
@@ -11,24 +11,17 @@ struct CoolScroll: View {
     let rectHeight: CGFloat = 50
 
     var body: some View {
-        ZStack(alignment: .bottom) {
-            ScrollView {
-                ForEach(1 ... 100, id: \.self) { index in
-                    Text("\(index)")
-                        .font(.title)
-                        .foregroundStyle(.tertiary)
-                        .monospaced()
-                        .frame(maxWidth: .infinity)
-                }
+        ScrollView {
+            ForEach(1 ... 100, id: \.self) { index in
+                Text("\(index)")
+                    .font(.title)
+                    .foregroundStyle(.tertiary)
+                    .monospaced()
+                    .frame(maxWidth: .infinity)
             }
-            .safeAreaInset(edge: .bottom) {
-                Color.clear.frame(height: rectHeight + 3)
-            }
-
-            Rectangle()
-                .fill(Color.red)
-                .opacity(0.8)
-                .frame(height: rectHeight)
+        }
+        .safeAreaInset(edge: .bottom) {
+            Color.red.opacity(0.8).frame(height: rectHeight + 3)
         }
     }
 }
